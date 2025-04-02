d83950a0412dda0c0aa2fe12fe3ef689c172c9e1
JuanD-20 <juanda.mosquera545@gmail.com>
2025-04-02 09:53:28 -0500

    Se cambia la NavBar para mover el boton de logout y las notificaciones

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../store/slices/userSlice';
@@ -7,9 +7,15 @@ import './NavBar.css';
 const NavBar = () => {
     const { isLoggedIn, user } = useSelector(state => state.user);
     const dispatch = useDispatch();
+    const [showDropdown, setShowDropdown] = useState(false);
 
     const handleLogout = () => {
         dispatch(logout());
+        setShowDropdown(false);
+    };
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
     };
 
     return (
@@ -21,29 +27,29 @@ const NavBar = () => {
                 <div className="navbar-welcome">
                     {isLoggedIn ? (
                         <div className="user-info">
-                            <div className="user-avatar">
+                            {isLoggedIn && (
+                                <button className="notifications-btn">
+                                    <span className="notification-icon">🔔</span>
+                                </button>
+                            )}
+                            <div className="user-avatar" onClick={toggleDropdown}>
                                 <img src={user.avatar || '../../images/defaultProfile.jpg'} alt="User Avatar" />
                             </div>
-                            <div className="user-name">
-                                <h3>Welcome!</h3>
-                                <p>{user.name}</p>
-                            </div>
+                            {showDropdown && (
+                                <div className="user-dropdown">
+                                    <ul>
+                                        <li><Link to="/profile">Mi Perfil</Link></li>
+                                        <li><Link to="/settings">Configuración</Link></li>
+                                        <li><button onClick={handleLogout}>Cerrar Sesión</button></li>
+                                    </ul>
+                                </div>
+                            )}
                         </div>
                     ) : (
                         <div className="login-buttons">
                             <Link to="/login" className="login-btn">Login</Link>
                             <Link to="/signup" className="signup-btn">Sign Up</Link>
                         </div>
-                    )}
-                    {isLoggedIn && (
-                        <button className="notifications-btn">
-                            <span className="notification-icon">🔔</span>
-                        </button>
-                    )}
-                    {isLoggedIn && (
-                        <button className="logout-btn" onClick={handleLogout}>
-                            Logout
-                        </button>
                     )}
                 </div>
             </div>
